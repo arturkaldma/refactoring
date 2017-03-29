11876103c305446af68424b3cf45216632d96f07
arturkaldma <arturka.kaldma@gmail.com>
2017-03-29 21:19:01 +0300

    Artur Kaldma

--- src/rental/RentalPriceCalculator.java
+++ src/rental/RentalPriceCalculator.java
@@ -8,32 +8,33 @@ public class RentalPriceCalculator {
 	// acc - has s/he caused any accidents within last year
 	// acc2 - has s/he participated (but not caused) in any accidents within last year
 	// season - if it is high season or not
-	public double price(int age, int licence, int clazz, boolean acc, boolean acc2, boolean season) {
+	public double CalculatePrice(int age, int yearsOfExperience, int classOfCar, boolean causedAccident, boolean participatedInAccident, boolean highSeason) {
 		
 		if (age < 18) {
 			throw new IllegalArgumentException("Driver too young - cannot quote the price");
 		}
-		if (age <= 21 && clazz > 2) {
+		if (age <= 21 && classOfCar > 2) {
 			throw new UnsupportedOperationException("Drivers 21 y/o or less can only rent Class 1 vehicles");
 		}
 		
 		double rentalprice = age;
 		
-		if (clazz >=4 && age <= 25 && season != false) {
+		if (classOfCar >=4 && age <= 25 && highSeason != false) {
 			rentalprice = rentalprice * 2;
 		}
 		
-		if (licence < 1) {
-			throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
+		if (yearsOfExperience < 1) {
+			throw new IllegalArgumentException("Driver must hold driving yearsOfExperience at least for one year. Can not rent a car!");
 		}
 		
-		if (licence < 3) {
+		if (yearsOfExperience < 3) {
 			rentalprice = rentalprice * 1.3;
 		}
 		
-		if (acc == true && age < 30) {
+		if (causedAccident == true && age < 30) {
 			rentalprice += 15;
 		}
+
 
 		if (rentalprice > 1000) {
 			return 1000.00;
